8a8094d3f83e8a4b8fd62040041e062ef9096229
Triston Pang <pang.triston@gmail.com>
2025-06-09 22:45:59 +0800

    feat: map service account into js object

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,28 @@ import { ApiBearerAuth, DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { initializeApp, ServiceAccount } from 'firebase-admin/app';
 import admin from 'firebase-admin';
-import * as serviceAccount from '../firebase-service-acc-key.json';
+// import * as serviceAccount from '../firebase-service-acc-key.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const serviceAccountFromEnv = {
+    type: process.env.FSA_TYPE,
+    project_id: process.env.FSA_PROJECT_ID,
+    private_key_id: process.env.FSA_PRIVATE_KEY_ID,
+    private_key: process.env.FSA_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FSA_CLIENT_EMAIL,
+    client_id: process.env.FSA_CLIENT_ID,
+    auth_uri: process.env.FSA_AUTH_URI,
+    token_uri: process.env.FSA_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FSA_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FSA_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FSA_UNIVERSE_DOMAIN,
+  };
+
   // Initialize Firebase Admin SDK
   initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert(serviceAccountFromEnv as ServiceAccount),
   });
 
   const makeDocument = () => {
